750f2f024548017560dc1c7151be00661044aa12
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2020-03-25 17:42:03 -0400

    SourceDonor changes

--- hyper.js
+++ hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 14,
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
@@ -146,7 +146,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'nord-hyper'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
