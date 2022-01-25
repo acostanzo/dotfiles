60874efc5d694030f11daad25b430532bb8ce1d3
Anthony Costanzo <memberships@acostanzo.com>
2022-01-25 17:18:07 -0500

    Configure telescope to be faster and more useful

--- config/nvim/plugin/telescope.lua
+++ config/nvim/plugin/telescope.lua
@@ -1,6 +1,44 @@
+local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
+local builtin = require("telescope.builtin")
+
 vim.api.nvim_set_keymap(
   'n',
   '<C-p>',
-  '<cmd>lua require("telescope.builtin").find_files()<cr>',
+  '<cmd>lua require("telescope.builtin").git_files()<cr>',
   {noremap = true}
 )
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    path_display = { "truncate" },
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = actions.which_key,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
