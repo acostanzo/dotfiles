ac1533547b6592c63daf418970c7344a126f46b4
Anthony Costanzo <memberships@acostanzo.com>
2022-01-26 09:56:58 -0500

    Use fzf-native in telescope

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -6,30 +6,31 @@ vim.cmd([[
   augroup end
 ]])
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself 
-  use 'wbthomason/packer.nvim' 
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- Utilities
-  use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'neovim/nvim-lspconfig' } -- Language Server
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- Tree Sitter syntax highlighting
-  use { 'scrooloose/nerdtree' }
-  use { 'vim-test/vim-test' }
-  use { 'terrortylor/nvim-comment' }
-  use { 'jose-elias-alvarez/null-ls.nvim' }
+	-- Utilities
+	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use({ "neovim/nvim-lspconfig" }) -- Language Server
+	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) -- Tree Sitter syntax highlighting
+	use({ "scrooloose/nerdtree" })
+	use({ "vim-test/vim-test" })
+	use({ "terrortylor/nvim-comment" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
 
-  -- Autocomplete
-  use { 'hrsh7th/cmp-buffer' }
-  use { 'hrsh7th/cmp-cmdline' }
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'hrsh7th/cmp-path' }
-  use { 'hrsh7th/nvim-cmp' }
-  use { 'dcampos/nvim-snippy' }
-  use { 'dcampos/cmp-snippy' }
+	-- Autocomplete
+	use({ "hrsh7th/cmp-buffer" })
+	use({ "hrsh7th/cmp-cmdline" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-path" })
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "dcampos/nvim-snippy" })
+	use({ "dcampos/cmp-snippy" })
 
-  -- Themes
-  use { 'EdenEast/nightfox.nvim' }
+	-- Themes
+	use({ "EdenEast/nightfox.nvim" })
 end)
