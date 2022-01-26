6c2af0643b6aa80061c95b5d949255eb6c4dc9db
Anthony Costanzo <memberships@acostanzo.com>
2022-01-26 16:52:16 -0500

    Get surround features back

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -22,6 +22,7 @@ return require("packer").startup(function(use)
 	use({ "terrortylor/nvim-comment" })
 	use({ "jose-elias-alvarez/null-ls.nvim" })
 	use({ "onsails/lspkind-nvim" })
+	use({ "blackCauldron7/surround.nvim" })
 
 	-- Autocomplete
 	use({ "hrsh7th/cmp-buffer" })
