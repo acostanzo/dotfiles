2c9005b1bc392a094c6e5f825553bab08f406026
Anthony Costanzo <memberships@acostanzo.com>
2022-02-16 12:20:54 -0500

    Add Octo for github in vim, update surround lib

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -14,7 +14,6 @@ return require("packer").startup(function(use)
 	use({ "kyazdani42/nvim-tree.lua", requires = { "kyazdani42/nvim-web-devicons" } })
 	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
 	use({ "neovim/nvim-lspconfig" }) -- Language Server
 	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) -- Tree Sitter syntax highlighting
@@ -22,7 +21,18 @@ return require("packer").startup(function(use)
 	use({ "terrortylor/nvim-comment" })
 	use({ "jose-elias-alvarez/null-ls.nvim" })
 	use({ "onsails/lspkind-nvim" })
-	use({ "blackCauldron7/surround.nvim" })
+	use({ "tpope/vim-surround" })
+
+	-- Git
+	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use({
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"kyazdani42/nvim-web-devicons",
+		},
+	})
 
 	-- Autocomplete
 	use({ "hrsh7th/cmp-buffer" })
