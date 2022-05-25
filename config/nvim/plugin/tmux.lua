096d162b19b8831f742a79d5749d48374b2eea6d
Anthony Costanzo <memberships@acostanzo.com>
2022-05-25 10:52:55 -0400

    Seamless tmux / vim window navigation

---
+++ config/nvim/plugin/tmux.lua
@@ -0,0 +1,10 @@
+require("tmux").setup({
+	navigation = {
+		-- enables default keybindings (C-hjkl) for normal mode
+		enable_default_keybindings = true,
+	},
+	resize = {
+		-- enables default keybindings (A-hjkl) for normal mode
+		enable_default_keybindings = true,
+	},
+})
