752011ae8f52a4fe3fb1a57a2faa96f3fc628f00
Anthony Costanzo <memberships@acostanzo.com>
2022-06-06 11:56:19 -0400

    Use eslint_d

--- config/nvim/plugin/null-ls.lua
+++ config/nvim/plugin/null-ls.lua
@@ -1,24 +1,28 @@
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 
+local code_actions = null_ls.builtins.code_actions
+local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-  formatting.prettierd,
-  formatting.stylua,
+	formatting.prettierd,
+	formatting.stylua,
+	code_actions.eslint_d,
+	diagnostics.eslint_d,
 }
 
 local on_attach = function(client)
-  if client.resolved_capabilities.document_formatting then
-    vim.cmd([[
+	if client.resolved_capabilities.document_formatting then
+		vim.cmd([[
       augroup LspFormatting
         autocmd! * <buffer>
         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
       augroup END
     ]])
-  end
+	end
 end
 
-null_ls.setup {
-  sources = sources,
-  on_attach = on_attach,
-}
+null_ls.setup({
+	sources = sources,
+	on_attach = on_attach,
+})
--- config/nvim/plugin/vim-test.lua
+++ config/nvim/plugin/vim-test.lua
@@ -4,3 +4,10 @@ KB("n", "<leader>s", ":TestNearest<CR>", { noremap = true, silent = true })
 KB("n", "<leader>l", ":TestLast<CR>", { noremap = true, silent = true })
 KB("n", "<leader>a", ":TestSuite<CR>", { noremap = true, silent = true })
 KB("n", "<leader>gt", ":TestVisit<CR>", { noremap = true, silent = true })
+
+vim.cmd([[
+  let g:test#basic#start_normal = 1 " If using basic strategy
+  let g:test#neovim#start_normal = 1 " If using neovim strategy
+  let test#javascript#executable = "yarn test"
+  let test#strategy = "vtr"
+]])
