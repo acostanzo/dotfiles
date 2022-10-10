ab00e1b4be6a7da24c0d343955335ebed0dfcc88
Anthony Costanzo <memberships@acostanzo.com>
2022-10-10 09:55:31 -0400

    Customize lualine

--- config/nvim/plugin/lualine.lua
+++ config/nvim/plugin/lualine.lua
@@ -1,8 +1,21 @@
 require("lualine").setup({
-	options = {
-		globalstatus = true,
-	},
-	sections = {
-		lualine_c = { "filename", "lsp_progress" },
-	},
+	-- options = {
+	-- 	globalstatus = true,
+	-- },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'filename'},
+    lualine_c = {'lsp_progress', 'diff', 'diagnostics'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'filename'},
+    lualine_c = {'lsp_progress', 'diff', 'diagnostics'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 })
