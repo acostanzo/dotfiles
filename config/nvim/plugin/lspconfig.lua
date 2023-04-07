26c78e113c6306cec4dbcd18da1d07250bf60b4d
Anthony Costanzo <memberships@acostanzo.com>
2023-04-07 00:33:58 -0400

    Format lua files

--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -7,67 +7,67 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
 
-	local function nmap(keyStroke, action, opts)
-		vim.api.nvim_buf_set_keymap(bufnr, "n", keyStroke, action, opts or { noremap = true, silent = true })
-	end
+  local function nmap(keyStroke, action, opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", keyStroke, action, opts or { noremap = true, silent = true })
+  end
 
-	-- Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	-- Normal mode key mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	nmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-	nmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-	nmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-	nmap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-	-- nmap("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-	nmap("<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
-	nmap("<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
-	nmap("<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
-	nmap("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-	nmap("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-	nmap("<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-	nmap("gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-	nmap("<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
-	nmap("[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
-	nmap("]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-	nmap("<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>")
-	nmap("<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>")
+  -- Normal mode key mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  nmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  nmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  nmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  nmap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  -- nmap("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  nmap("<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+  nmap("<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+  nmap("<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  nmap("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  nmap("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  nmap("<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  nmap("gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  nmap("<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
+  nmap("[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+  nmap("]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+  nmap("<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>")
+  nmap("<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>")
 
-	-- Auto format specific clients on save
-	if TableContains({ "eslint", "sumneko_lua" }, client.name) then
-		vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
-	end
+  -- Auto format specific clients on save
+  if TableContains({ "eslint", "sumneko_lua" }, client.name) then
+    vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+  end
 end
 
 local settings = {
-	sumneko_lua = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim", "KB", "TableContains" },
-			},
-		},
-	},
+  sumneko_lua = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim", "KB", "TableContains" },
+      },
+    },
+  },
 }
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local clients = {
-	eslint = true,
-	solargraph = true,
-	sumneko_lua = true,
-	tsserver = true,
+  eslint = true,
+  solargraph = true,
+  sumneko_lua = true,
+  tsserver = true,
 }
 for client, enabled in pairs(clients) do
-	if enabled then
-		nvim_lsp[client].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = settings[client],
-		})
-	end
+  if enabled then
+    nvim_lsp[client].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = settings[client],
+    })
+  end
 end
--- config/nvim/plugin/null-ls.lua
+++ config/nvim/plugin/null-ls.lua
@@ -2,27 +2,14 @@ local null_ls = require("null-ls")
 
 local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
-local formatting = null_ls.builtins.formatting
+-- local formatting = null_ls.builtins.formatting
 
 local sources = {
-	diagnostics.rubocop,
-	formatting.stylua,
 	code_actions.cspell,
 	diagnostics.cspell,
+	diagnostics.rubocop,
 }
-
-local on_attach = function(client)
-	if client.server_capabilities.document_formatting then
-		vim.cmd([[
-      augroup LspFormatting
-        autocmd! * <buffer>
-        autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-      augroup END
-    ]])
-	end
-end
 
 null_ls.setup({
 	sources = sources,
-	on_attach = on_attach,
 })
