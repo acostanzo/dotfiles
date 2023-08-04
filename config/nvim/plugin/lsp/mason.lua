05389206472709d523436f1773d30bb93c18d203
Anthony Costanzo <memberships@acostanzo.com>
2023-08-04 10:44:58 -0400

    Fix client install

--- config/nvim/plugin/lsp/mason.lua
+++ config/nvim/plugin/lsp/mason.lua
@@ -1,4 +1,9 @@
 local clients = dofile(CWD() .. "clients.lua")
 
+local client_names = {}
+for client, _ in pairs(clients) do
+  table.insert(client_names, client)
+end
+
 require("mason").setup()
-require("mason-lspconfig").setup({ ensure_installed = clients })
+require("mason-lspconfig").setup({ ensure_installed = client_names })
