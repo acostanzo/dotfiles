1dc273994e618037643d5304906ebb9e50b4c18b
Anthony Costanzo <memberships@acostanzo.com>
2024-05-28 14:50:00 -0400

    Bring back projectionist

--- config/nvim/lua/acostanzo/plugins/init.lua
+++ config/nvim/lua/acostanzo/plugins/init.lua
@@ -2,4 +2,5 @@ return {
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 	"jghauser/mkdir.nvim", -- mkdir -p on save if it doesn't exist
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"tpope/vim-projectionist", -- :A for alternate file (test file / code file)
 }
