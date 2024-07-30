27667f9a518872ec73de3cb2587376ea50b2e8f6
Anthony Costanzo <mail@acostanzo.com>
2024-07-30 12:20:30 -0400

    feat(nvim): add CopilotChat plugin configuration
    - Added CopilotChat.nvim plugin with canary branch.
    - Included dependencies: copilot.lua and plenary.nvim.
    - Enabled debug mode in plugin options.
    - Configured key mappings for help and prompt actions using Telescope.

---
+++ config/nvim/lua/plugin_configs/copilotChat.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		opts = {
+			debug = true,
+		},
+		keys = {
+			{
+				"<leader>cch",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+				end,
+				desc = "CopilotChat - Help actions",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>ccp",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+				end,
+				desc = "CopilotChat - Prompt actions",
+				mode = { "n", "v" },
+			},
+		},
+	},
+}
